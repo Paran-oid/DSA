21bb7624e31407eb2baddc578e5d037b1a1a1d06
Paran-oid <azizhmid10000@gmail.com>
2025-02-11 17:02:09 +0100

    test

--- main.c
+++ main.c
@@ -9,12 +9,6 @@ int main(void) {
 
   init_logger(ALL);
 
-  Matrix mat = init_mat(3, 3);
-  i32 arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-  fill_mat(&mat, arr, sizeof(arr) / sizeof(arr[0]));
-  print_mat(&mat);
-
   destroy_logger();
-
   exit(EXIT_SUCCESS);
 }
