f33f87192c6af9377f42724fa7f4adc3a5de2738
Paran-oid <azizhmid10000@gmail.com>
2025-02-19 15:45:07 +0100

    refactor: remove commented-out code and address FIXMEs and TODOs in main function

--- main.c
+++ main.c
@@ -12,21 +12,9 @@ int main(void) {
 
   logger_init(ALL);
 
-  i32 x = 3;
-  i32 y = 4;
-  i32 z = 5;
-  i32 a = 6;
-  i32 b = 7;
-  i32 c = 8;
-  tree_t *tree = tree_init(INT);
-  tree_insert(tree, &x);
-  tree_insert(tree, &y);
-  tree_insert(tree, &z);
-  tree_insert(tree, &a);
-  tree_insert(tree, &b);
-  tree_insert(tree, &c);
-
-  printf("value of root is %d\n", *(i32 *)tree->root->right->val);
+  // test stack and queue
+  // deal with all the FIXMES
+  // get rid of all TODOS and commented out code
 
   logger_destroy();
   exit(EXIT_SUCCESS);
