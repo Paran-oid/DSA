7b2cdfb05563d3522e6c2cfd57cdcbecc261e25d
Paran-oid <azizhmid10000@gmail.com>
2025-03-18 21:51:28 +0100

    feat: implement binary search tree (BISTree) functionalities; add creation, insertion, lookup, and removal functions; update main for testing

--- main.c
+++ main.c
@@ -1,7 +1,83 @@
+#include "bistree.h"
+#include "btree.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
+void destroy(void* data)
 {
-    printf("hello world\n");
+    free(data); // Free dynamically allocated memory
+}
+
+int match(const void* key1, const void* key2)
+{
+    return (*(int*)key1 - *(int*)key2);
+}
+
+// Helper function to insert an integer into the AVL tree
+void insert_test(AvlTree* tree, int value)
+{
+    int* data = malloc(sizeof(int));
+    *data = value;
+    if (bistree_insert(tree, (void*)data) == 0) {
+        printf("Inserted: %d\n", value);
+    } else {
+        printf("Failed to insert: %d\n", value);
+        free(data); // Prevent memory leak
+    }
+}
+
+// Helper function to look up an integer
+void lookup_test(AvlTree* tree, int value)
+{
+    int* data = &value;
+
+    if (bistree_lookup(tree, (void**)&data) == 0) {
+        printf("Found: %d\n", *data);
+    } else {
+        printf("Not found: %d\n", value);
+    }
+}
+
+// Helper function to remove an integer
+void remove_test(AvlTree* tree, int value)
+{
+    if (bistree_remove(tree, &value) == 0) {
+        printf("Removed: %d\n", value);
+    } else {
+        printf("Failed to remove: %d\n", value);
+    }
+}
+
+int main()
+{
+    AvlTree tree;
+
+    // Initialize the AVL tree
+    bistree_create(&tree, destroy, match);
+
+    // Insert test values
+    insert_test(&tree, 50);
+    insert_test(&tree, 30);
+    insert_test(&tree, 70);
+    insert_test(&tree, 20);
+    insert_test(&tree, 40);
+    insert_test(&tree, 60);
+    insert_test(&tree, 80);
+
+    printf("\nTree size: %d\n\n", bistree_size(&tree));
+
+    // Look up values
+    lookup_test(&tree, 30);
+    lookup_test(&tree, 100); // Should not be found
+
+    // Remove values
+    remove_test(&tree, 30);
+    remove_test(&tree, 100); // Should fail
+
+    printf("\nTree size after deletion: %d\n", bistree_size(&tree));
+
+    // Destroy the tree
+    bistree_destroy(&tree);
+
     return 0;
 }
--- tree/bistree.c
+++ tree/bistree.c
@@ -95,9 +95,9 @@ static void left_destroy(BiTree* tree, BiTreeNode* node)
     }
 
     if (node == NULL) {
-        *position = bitree_root(tree);
+        position = &bitree_root(tree);
     } else {
-        *position = bitree_left(node);
+        position = &bitree_left(node);
     }
 
     if (*position != NULL) {
@@ -108,8 +108,7 @@ static void left_destroy(BiTree* tree, BiTreeNode* node)
         }
     }
 
-    free((*position)->data);
-    free(*position);
+    tree->destroy(*position);
     *position = NULL;
 
     tree->size--;
@@ -123,9 +122,9 @@ static void right_destroy(BiTree* tree, BiTreeNode* node)
     }
 
     if (node == NULL) {
-        *position = bitree_root(tree);
+        position = &bitree_root(tree);
     } else {
-        *position = node;
+        position = &bitree_right(node);
     }
 
     if (*position != NULL) {
@@ -141,21 +140,161 @@ static void right_destroy(BiTree* tree, BiTreeNode* node)
         }
     }
 
-    free((*position)->data);
     free(*position);
     *position = NULL;
 
     tree->size--;
 }
 
-static int insert(BiTree* tree, BiTreeNode* node, const void* data, int* balanced)
+static int insert(BiTree* tree, BiTreeNode** node, const void* data, int* balanced)
 {
+    AvlNode* avl_data;
+    int cmp_val, ret_val;
+
+    if (bitree_is_eob(*node)) {
+        if ((avl_data = (AvlNode*)malloc(sizeof(AvlNode))) == NULL) {
+            return -1;
+        }
+        avl_data->factor = AVL_BALANCED;
+        avl_data->data = (void*)data;
+        avl_data->hidden = 0;
+
+        if ((ret_val = bitree_ins_left(tree, *node, avl_data)) != 0) {
+            return -1;
+        }
+        *balanced = 0;
+    } else {
+        cmp_val = tree->match(data, ((AvlNode*)bitree_data(*node))->data);
+        if (cmp_val < 0) {
+            if (bitree_is_eob(bitree_left(*node))) {
+                if ((avl_data = (AvlNode*)malloc(sizeof(AvlNode))) == NULL) {
+                    return -1;
+                }
+                avl_data->data = (void*)data;
+                avl_data->hidden = 0;
+                avl_data->factor = AVL_BALANCED;
+
+                if (bitree_ins_left(tree, *node, avl_data) == -1) {
+                    return -1;
+                }
+                *balanced = 0;
+            } else {
+                if ((ret_val = insert(tree, &bitree_left(*node), data, balanced)) != 0) {
+                    return ret_val;
+                }
+            }
+
+            if (!(*balanced)) {
+                switch (((AvlNode*)bitree_data(*node))->factor) {
+                case AVL_LFT_HEAVY:
+                    rotate_left(node);
+                    *balanced = 1;
+                    break;
+                case AVL_BALANCED:
+                    ((AvlNode*)bitree_data(*node))->factor = AVL_LFT_HEAVY;
+                    break;
+                case AVL_RGT_HEAVY:
+                    ((AvlNode*)bitree_data(*node))->factor = AVL_BALANCED;
+                    *balanced = 1;
+                    break;
+                }
+            }
+        } else if (cmp_val > 0) {
+            if (bitree_is_eob(bitree_right(*node))) {
+                if ((avl_data = (AvlNode*)malloc(sizeof(AvlNode))) == NULL) {
+                    return -1;
+                }
+                avl_data->data = (void*)data;
+                avl_data->factor = AVL_BALANCED;
+                avl_data->hidden = 0;
+
+                if (bitree_ins_right(tree, *node, avl_data) != 0) {
+                    return -1;
+                }
+
+                *balanced = 0;
+            } else {
+                if ((ret_val = insert(tree, &bitree_right(*node), data, balanced)) != 0) {
+                    return ret_val;
+                }
+            }
+
+            if (!(*balanced)) {
+                switch (((AvlNode*)bitree_data(*node))->factor) {
+                case AVL_RGT_HEAVY:
+                    rotate_right(node);
+                    *balanced = 1;
+                    break;
+                case AVL_BALANCED:
+                    ((AvlNode*)bitree_data(*node))->factor = AVL_RGT_HEAVY;
+                    break;
+                case AVL_LFT_HEAVY:
+                    ((AvlNode*)bitree_data(*node))->factor = AVL_BALANCED;
+                    break;
+                }
+            }
+        } else {
+            if (!((AvlNode*)bitree_data(*node))->hidden) {
+                return 1;
+            } else {
+                if (tree->destroy) {
+                    tree->destroy(((AvlNode*)bitree_data(*node))->data);
+                }
+
+                ((AvlNode*)bitree_data(*node))->data = (void*)data;
+                ((AvlNode*)bitree_data(*node))->hidden = 0;
+
+                *balanced = 1;
+            }
+        }
+    }
+    return 0;
 }
 static int hide(BiTree* tree, BiTreeNode* node, const void* data)
 {
+    int cmp_val, ret_val;
+
+    if (bitree_is_eob(node)) {
+        return -1;
+    }
+
+    cmp_val = tree->match(data, ((AvlNode*)bitree_data(node))->data);
+
+    if (cmp_val < 0) {
+        ret_val = hide(tree, bitree_left(node), data);
+    } else if (cmp_val > 0) {
+        ret_val = hide(tree, bitree_right(node), data);
+    } else {
+        ((AvlNode*)bitree_data(node))->hidden = 1;
+        ret_val = 0;
+    }
+
+    return ret_val;
 }
 static int lookup(BiTree* tree, BiTreeNode* node, void** data)
 {
+    int ret_val, cmp_val;
+
+    if (bitree_is_eob(node)) {
+        return -1;
+    }
+
+    cmp_val = tree->match(*data, ((AvlNode*)bitree_data(node))->data);
+
+    if (cmp_val < 0) {
+        ret_val = lookup(tree, bitree_left(node), data);
+    } else if (cmp_val > 0)
+        ret_val = lookup(tree, bitree_right(node), data);
+    else {
+        if (!((AvlNode*)bitree_data(node))->hidden) {
+            *data = ((AvlNode*)bitree_data(node))->data;
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+
+    return ret_val;
 }
 
 void bistree_create(AvlTree* tree, void (*destroy)(void* data), int (*match)(const void* key1, const void* key2))
